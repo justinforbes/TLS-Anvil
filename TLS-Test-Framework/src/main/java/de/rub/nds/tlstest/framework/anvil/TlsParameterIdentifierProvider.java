bacdd4ec758bf8027da93756e7b318a6f3366a63
mmaehren <32199075+mmaehren@users.noreply.github.com>
2025-09-04 09:17:38 +0200

    Fixed NPE for non-co runs

--- TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/anvil/TlsParameterIdentifierProvider.java
+++ TLS-Test-Framework/src/main/java/de/rub/nds/tlstest/framework/anvil/TlsParameterIdentifierProvider.java
@@ -63,24 +63,27 @@ public class TlsParameterIdentifierProvider extends ParameterIdentifierProvider 
 
     private void generateAllConfigOptionParameters(
             List<ParameterIdentifier> identifiers, String anvilContextId) {
-        for (ConfigOptionParameterType listed : ConfigOptionParameterType.values()) {
-            if (listed == ConfigOptionParameterType.CONFIG_OPTION_COMPOUND_PARAMETER) {
-                identifiers.add(new ParameterIdentifier(listed, ParameterScope.NO_SCOPE));
-            } else if (listed == ConfigOptionParameterType.COMMON_BUILD_FLAG) {
-                // upon the first iteration, the config option mappings may not have been
-                // initialized yet
-                ConfigurationOptionsConfig coConfig =
-                        TestContextRegistry.getContext(anvilContextId)
-                                .getConfigurationOptionsExtension()
-                                .getConfig();
-                if (coConfig != null) {
-                    // all common options will be added under distinct scopes
-                    identifiers.addAll(coConfig.getEnabledConfigOptionDerivations());
+        if (TestContextRegistry.getContext(anvilContextId).getConfigurationOptionsExtension()
+                != null) {
+            for (ConfigOptionParameterType listed : ConfigOptionParameterType.values()) {
+                if (listed == ConfigOptionParameterType.CONFIG_OPTION_COMPOUND_PARAMETER) {
+                    identifiers.add(new ParameterIdentifier(listed, ParameterScope.NO_SCOPE));
+                } else if (listed == ConfigOptionParameterType.COMMON_BUILD_FLAG) {
+                    // upon the first iteration, the config option mappings may not have been
+                    // initialized yet
+                    ConfigurationOptionsConfig coConfig =
+                            TestContextRegistry.getContext(anvilContextId)
+                                    .getConfigurationOptionsExtension()
+                                    .getConfig();
+                    if (coConfig != null) {
+                        // all common options will be added under distinct scopes
+                        identifiers.addAll(coConfig.getEnabledConfigOptionDerivations());
+                    }
+                } else {
+                    // all CO types except for the compound parameter are used in a separate IPM
+                    identifiers.add(
+                            new ParameterIdentifier(listed, ConfigOptionParameterScope.DEFAULT));
                 }
-            } else {
-                // all CO types except for the compound parameter are used in a separate IPM
-                identifiers.add(
-                        new ParameterIdentifier(listed, ConfigOptionParameterScope.DEFAULT));
             }
         }
     }
